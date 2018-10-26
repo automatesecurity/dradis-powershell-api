aa33b22250b4630efb236907a1a79ee8cf6b0dcd
Daniel Wood <daniel.wood@owasp.org>
2018-10-25 21:58:46 -0400

    Complete switch functions

--- dradis-powershell-api.ps1
+++ dradis-powershell-api.ps1
@@ -23,7 +23,11 @@
 
 
 # Setup Variables
-# $token = 'XXXXXXXXXX' # Use your own API token here
+$token = 'XXXXXXXXXX' # Use your own API token here
+$headers = @{ "Authorization" = "Token $token"}
+$url = https://dradis.url
+$api = '/pro/api/' # TODO: endpoint array for team, projects, issues, and clients
+
 function Invoke-Menu
 {
     param (
@@ -44,19 +48,50 @@ Invoke-Menu
 $selection = Read-Host "Please make a selection"
 switch ($selection) {
     '1' {
-
+        # List all projects within the database
+        $response = Invoke-RestMethod -Method Get -Uri $url/$api -Headers $headers -ContentType "application/json"
+        $response | Select-Object ID,Name,created_at,authors
     }
     '2' {
-
+        # List all findings within a specific project
+        $project = Read-Host "Please enter a project ID"
+        $response = Invoke-RestMethod -Method Get -Uri $url -Headers @{ "Authorization" = "Token $token"; "Dradis-Project-Id" = $project} -ContentType "application/json"
+        $response | Select-Object -ExpandProperty fields | Format-Table -AutoSize # | Export-Csv -Path "\\local\path\output.csv"
     }
     '3' {
-
+        # Create a new project
+        Write-Host ""
+        $name = Read-Host "What is the name of the new project?"
+        Write-Host "Now creating $name"
+        $project = @{
+            name = $name
+            client_id='1' # customize this for the client the project is associated with
+        }
+        $json = $project | ConvertTo-Json
+        $response = Invoke-RestMethod -Method Post -Uri $url -Headers $headers -Body $json -ContentType "application/json"
     }
     '4' {
-
+        # Edit an existing project
+        Write-Host ""
+        $id = Read-Host "What is the id of the project you want to edit?" # TODO: Optionality for id or name
+        $name = Read-Host "Please enter the new name for the project..."
+        $project = @{
+            name = $name
+        }
+        $json = $project | ConvertTo-Json
+        $response = Invoke-RestMethod -Method Put -Uri $url $id -Headers $headers -Body $json -ContentType "application/json"
     }
     '5' {
-
+        # Delete a project
+        Write-Host ""
+        Write-Warning "Be VERY CAREFUL DOING THIS - CHANGES ARE PERMANENT"
+        $id = Read-Host "What Project ID do you want to delete from the database?"
+        Write-Host "Now deleting Project $id"
+        $project = @{
+            id = $id
+        }
+        $json = $project | ConvertTo-Json
+        $request = Invoke-RestMethod -Method Delete -Uri $url $id -Headers $headers -Body $json -ContentType "application/json"
     }
     'Q' {
         Write-Host "Quitting" -ForegroundColor Red
